f21d95340753c440c4e2b2965fff3c8e678a5359
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-04-22 00:09:04 -0400

    Intro to debouncing, refactored search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,18 @@ import axios from "axios"
 
 const Search = () => {
   const [term, setTerm] = useState("programming")
+  const [debouncedTerm, setDebouncedTerm] = useState(term)
   const [results, setResults] = useState([])
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term)
+    }, 1000)
+
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [term])
 
   useEffect(() => {
     const search = async () => {
@@ -13,23 +24,16 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term
+          srsearch: debouncedTerm
         }
       })
 
       setResults(data.query.search)
     }
-
-    const timeoutId = setTimeout(() => {
-      if (term) {
-        search()
-      }
-    }, 1000)
-
-    return () => {
-      clearTimeout(timeoutId)
+    if (debouncedTerm) {
+      search()
     }
-  }, [term])
+  }, [debouncedTerm])
 
   const renderedResults = results.map((result) => {
     return (
